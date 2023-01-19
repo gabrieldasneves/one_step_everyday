949acec5893a08e631be0374fd9740a2d733fb8d
Gabriel das Neves <galvesdasneves@gmail.com>
2023-01-19 20:51:05 -0300

    isolando arquivos backend

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -1,18 +1,13 @@
 import Fastify from "fastify"
-import {PrismaClient} from '@prisma/client'
-import  from '@fastify/cors'
+import { appRoutes } from "./routes"
+import cors from '@fastify/cors'
+
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
-
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany()
-
-    return habits
-})
-
+app.register(appRoutes)
 app.listen({
     port:3333,
 }).then(() => {
