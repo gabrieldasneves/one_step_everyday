487784fc8e21e2ce875b25349f33090869b65aca
Gabriel das Neves <galvesdasneves@gmail.com>
2023-01-21 00:10:21 -0300

    fixing bug

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,10 @@
-interface HabitProps{
+interface HabitDayProps{
     completed: number
 }
 
-function Habits(props: HabitProps){
+export function HabitDay(props: HabitDayProps){
     return(
-        <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center">{props.completed}</div>
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
     )
 }
 
-export default Habits
