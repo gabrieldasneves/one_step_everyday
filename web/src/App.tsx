f2166e356894687894f30d8b4f550ddac15b7eb5
Gabriel das Neves <galvesdasneves@gmail.com>
2023-01-21 00:08:47 -0300

    creating other components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 //import Habits from "./components/Habits"
 import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 import './styles/global.css';
 
 
@@ -8,6 +9,7 @@ export function App() {
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full maw-w-5xl px-6 flex flex-col gap-16'>
         <Header />
+        <SummaryTable />
       </div>
     </div>
   )
