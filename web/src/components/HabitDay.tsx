c7b37dbb31c89e34f8c7aeadc2255624dd54ba4c
Gabriel das Neves <galvesdasneves@gmail.com>
2023-01-23 23:06:17 -0300

    filling more days with empty squares

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -2,9 +2,10 @@ interface HabitDayProps{
     completed: number
 }
 
-export function HabitDay(props: HabitDayProps){
-    return(
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+function HabitDay(props: HabitDayProps){
+    return (
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
     )
 }
 
+export default HabitDay
