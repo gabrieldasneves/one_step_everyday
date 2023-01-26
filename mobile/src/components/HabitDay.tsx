22e184cc1776b5e814de48cf09279aae9b2f125e
Gabriel das Neves <galvesdasneves@gmail.com>
2023-01-26 16:39:32 -0300

    generating range of empty dates + scroll view setup

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -8,6 +8,6 @@ export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - SCREEN_HO
 
 export function HabitDay() {
     return (
-        <TouchableOpacity className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"  style={{width:DAY_SIZE, height:DAY_SIZE}}/>
+        <TouchableOpacity className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800" activeOpacity={0.7}  style={{width:DAY_SIZE, height:DAY_SIZE}}/>
     )
 }
