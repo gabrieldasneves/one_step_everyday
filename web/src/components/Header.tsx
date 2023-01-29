e8aef55402d58609d3768215f0a3cfd6261183be
Gabriel das Neves <galvesdasneves@gmail.com>
2023-01-29 16:17:20 -0300

    useState approach

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,14 +1,29 @@
 import logoImage from '../assets/logo.svg';
 import {Plus} from 'phosphor-react';
+import { useState } from 'react';
+
+
 
 export function Header(){
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  function buttonClicked(){
+    setIsModalOpen(true)
+  }
+
     return (
         <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          
           <img src={logoImage} alt="" />
-          <button type="button" className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
+
+          {isModalOpen? <p>oi</p> : null}
+
+          <button onClick={buttonClicked} type="button" className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
             <Plus size={20} className="text-violet-500"/>
             Novo hábito
           </button>
+
         </div>
     )
 }
